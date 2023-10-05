e347a54b8264f9b3a25d6b7a3eef7eefef2fb3db
SIMWOOHYUN <75990406+neverleaveualong@users.noreply.github.com>
2023-10-05 17:59:08 +0900

    Add files via upload

---
+++ SequentialSearch.py
@@ -0,0 +1,10 @@
+#순차탐색
+#SeqyentialSearch
+
+def seqsearch (n, S, x):
+    location = 1
+    while (location <= n and S[location] != x):
+        location += 1 
+    if (location > n):
+        location = 0 
+    return location
