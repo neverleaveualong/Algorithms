f04739da0061b82727307a2d3cb33b9e8e74b594
SIMWOOHYUN <75990406+neverleaveualong@users.noreply.github.com>
2023-11-03 22:26:52 +0900

    BinarySearch(Java)

---
+++ BinarySearch.java
@@ -0,0 +1,45 @@
+package practice;
+
+public class BinarySearch {
+	
+	
+	public static int binarysearch(int[] arr, int x){
+		
+		int low = 1;
+		int high = arr.length;
+		int location = -1;
+		
+		while (low <= high) {
+			int middle = (low + high) / 2;
+			
+			if (arr[middle] == x) {
+				location = middle;
+				break;
+			}
+			else if (arr[middle] > x) {
+				high = middle - 1;
+			}
+			else  {
+				low = middle + 1;
+			}
+		}
+		
+		return location;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,3,5,7,9,11,13,20,30};
+		int findvalue = 5;
+		
+		int resultvalue = binarysearch(arr, findvalue);
+		
+		if (resultvalue == -1) {
+			System.out.println("찾고자하는 값 "+findvalue+"는 배열에 존재하지 않는 값입니다.");
+		}
+		else {
+			System.out.println("찾고자하는 값 "+findvalue +"는 배열의"+(resultvalue+1)+"번째 위치해 있습니다.");
+		}
+	}
+}
